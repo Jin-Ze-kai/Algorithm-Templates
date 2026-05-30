c1c9b6e2667501698cdcf9c72070bab3a88bf35d
MoskenMaelstrom <485106943@qq.com>
2026-05-30 16:51:28 +0800

    Auto Update

--- 板子/字符串/01Trie.cpp
+++ 板子/字符串/01Trie.cpp
@@ -1,41 +1,34 @@
 
-constexpr int N=1e5*31;
-int tot;
-int trie[N][2];
-int cnt[N];
+const int N=1e5*32;
+int tree[N][2];
+int pass[N];
+int len=1;
 
-int newNode() {
-	int x=++tot;
-	trie[x][0]=trie[x][1]=0;
-	cnt[x]=0;
-	return x;
-}
-
-void init() {
-	tot=0;
-	newNode();
-}
-
-void add(int x,int t) {
-	int o=1;
-	for(int i=30;i>=0;i--) {
-		int &p=trie[o][x>>i&1];
-		if (p == 0) p=newNode();
-		o=p;
-		cnt[o]+=t;
-	}
+void insert(int x) {
+    int cur=0;
+    pass[cur]++;
+    for(int i=30;i>=0;i--) {
+        int b=(x>>i)&1;
+        if (tree[cur][b] == 0) {
+            tree[len][0]=tree[len][1]=0;
+            pass[len]=0;
+            tree[cur][b]=len++;
+        }
+        cur=tree[cur][b];
+        pass[cur]++;
+    }
 }
 
 int query(int x) {
-	int o=1;
-	int ans=0;
-	for(int i=30;i>=0;i--) {
-		int d=x>>i&1;
-		if (cnt[trie[o][d^1]]) {
-			ans|=1<<i;
-			d^=1;
-		}
-		o=trie[o][d];
-	}
-	return ans;
+    int cur=0;
+    int ans=0;
+    for(int i=30;i>=0;i--) {
+        int b=(x>>i)&1;
+        if (tree[cur][b^1] != 0) {
+            ans|=(1<<i);
+            cur=tree[cur][b^1];
+        }
+        else cur=tree[cur][b];
+    }
+    return ans;
 }
